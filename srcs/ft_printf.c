ce444a214d5a3fd3ebf251c93dec03bb6a82c92e
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-12-18 23:35:01 +0300

    Probably working skeleton for ft_printf()

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <oadhesiv@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 22:44:37 by sselusa           #+#    #+#             */
-/*   Updated: 2019/12/18 21:00:21 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/12/18 23:34:42 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int					ft_printf(const char *format, ...)
 {
-	t_field			*head;
+	char			*cur;
+	t_field			*dummy;
+	t_list			*head;
+	int				ret_len;
+	char			*ret_str;
 
-	head = creare_field("");
-	input(&head, &format);
-	ft_putstr("F\n");
-	ft_putstr(format);
-	exit(0);
-	return (0);
+	dummy = creare_field("");
+	g_head = ft_lstnew((void*)dummy, sizeof(t_field));
+	cur = format;
+	input((char**)&cur);
+	ret_str = ft_strnew(0);
+	output(&ret_str, &ret_len);
+	ft_putstr(ret_str);
+	return (ret_len);
 }
